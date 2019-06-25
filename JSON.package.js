7a720d9bc48461e9a9b19512efefa09b914ac178
Rich Dutton <48740087+rcliffd@users.noreply.github.com>
2019-06-25 16:16:35 -0500

    Add files via upload
    this whole file is filled with errors and I have to work them out. Now that we learned that quick debugger fix in the future when I fix this I hope that is able to work, yet at the moment I have been going back and forth and having issues with my package uploads...

---
+++ JSON.package.js
@@ -0,0 +1,24 @@
+
+{
+  "name": "liri-app",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/rcliffd/LiRi-word"
+  },
+
+  "homepage": "https://github.com/rcliffd/LiRi-word-app#readme",
+  "dependencies": {
+    "axios": "^0.19.0",
+    "chalk": "^2.4.2",
+    "chalk-animation": "^1.6.0",
+    "dotenv": "^8.0.0",
+    "moment": "^2.24.0",
+    "node-spotify-api": "^1.1.1"
+  }
+}
