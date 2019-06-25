f02458d4553a8707f954331ebb7aa7f7f4f26cb1
Rich Dutton <48740087+rcliffd@users.noreply.github.com>
2019-06-25 16:17:59 -0500

    Update JSON.package.js

--- JSON.package.js
+++ JSON.package.js
@@ -12,13 +12,5 @@
     "url": "git+https://github.com/rcliffd/LiRi-word"
   },
 
-  "homepage": "https://github.com/rcliffd/LiRi-word-app#readme",
-  "dependencies": {
-    "axios": "^0.19.0",
-    "chalk": "^2.4.2",
-    "chalk-animation": "^1.6.0",
-    "dotenv": "^8.0.0",
-    "moment": "^2.24.0",
-    "node-spotify-api": "^1.1.1"
-  }
+  
 }
